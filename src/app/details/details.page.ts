5591692a58183cddc6a47578de3673db1d8ff820
Vero <dipmveronica@gmail.com>
2024-05-08 20:17:25 -0300

    feat: added navbar to detail section

--- src/app/details/details.page.ts
+++ src/app/details/details.page.ts
@@ -1,22 +1,35 @@
-import { Component, WritableSignal, inject, signal } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, Input, WritableSignal, inject, signal } from '@angular/core';
+import { CommonModule, DatePipe, CurrencyPipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonIcon, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonText, IonLabel, IonButtons, IonBackButton, IonItem } from '@ionic/angular/standalone';
 import { MovieService } from '../services/movie.service';
 import { MovieResultI } from '../services/interfaces';
+import { cashOutline, calendarOutline } from "ionicons/icons"
+import { addIcons } from 'ionicons';
+
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.page.html',
   styleUrls: ['./details.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonIcon, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonText, IonLabel, IonButtons, IonBackButton, IonItem, CommonModule, FormsModule, DatePipe, CurrencyPipe]
 })
 export class DetailsPage {
   private movieService = inject(MovieService);
   imageBaseURL = "https://image.tmdb.org/t/p";
   movie: WritableSignal<MovieResultI | null> = signal(null);
 
-  constructor() { }
+  @Input()
+  set id(movieID: string){
+    this.movieService.getMovieDetails(movieID).subscribe((movie) => {
+      this.movie.set(movie)
+    })
+  }
+
+  constructor() { 
+    addIcons({cashOutline, calendarOutline});
+  }
 
 }
+ 
